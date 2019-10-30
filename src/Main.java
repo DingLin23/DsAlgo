aab7e3d100d3688223b7e0ace03d53d4f4947ebb
dinglin <tim920323@gmail.com>
2019-10-30 16:18:14 -0400

    array

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.sql.SQLOutput;
 
 public class Main {
+    public static void main(String[] args) {
+        Array arr = new Array(20);
+        for (int i = 0; i < 10; i++) {
+            arr.addLast(i);
+        }
+        arr.add(3, 6);
+        System.out.println(arr);
+
+        arr.addFirst(122);
+        System.out.println(arr);
+
+        System.out.println(arr.get(2));
+        arr.set(2,10);
+        System.out.println(arr.get(2));
+    }
 }
